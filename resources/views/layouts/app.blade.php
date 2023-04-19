82a05ac4938dfdb4b9e5efd4a6a491423b5556ba
Vũ  Việt Anh <vuvietanh@192.168.55.101>
2023-04-19 23:42:20 +0700

    fix login and breeze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,9 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+        <script src="{{asset("js/app.js")}}"></script>
+        <link rel="stylesheet" href="{{asset("css/app.css")}}">
+?    </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
