e31cb690566b46e0d7b75563fa2afbab5f90292e
Vũ  Việt Anh <vuvietanh@MacBook-Air-cua-Vu.local>
2023-04-21 16:48:30 +0700

    add comment, add link on user.layout

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Movflx - Online Movies & TV Shows Template</title>
-    <meta name="description" content="">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta charset="utf-8" />
+    <meta content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta name="theme-color" content="#234556">
+    <meta http-equiv="Content-Language" content="vi" />
+    <meta content="VN" name="geo.region" />
+    <meta name="DC.language" scheme="utf-8" content="vi" />
+    <meta name="language" content="Việt Nam">
+
 
     <link rel="shortcut icon" href="https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png" type="image/x-icon" />
     <meta name="revisit-after" content="1 days" />
@@ -187,6 +190,9 @@
 <div id='easy-top'></div>
 <!--end footer-->
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"  ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"  ></script>
+
 <script type='text/javascript' src={{asset('user/js/bootstrap.min.js?ver=5.7.2')}} id='bootstrap-js'></script>
 <script type='text/javascript' src={{asset('user/js/owl.carousel.min.js?ver=5.7.2')}} id='carousel-js'></script>
 
@@ -195,29 +201,80 @@
 
 
 
-<!-- JS here -->
-<script src="{{asset('js/vendor/jquery-3.6.0.min.js')}}"></script>
-<script src="{{asset('js/popper.min.js')}}"></script>
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/isotope.pkgd.min.js')}}"></script>
-<script src="{{asset('js/imagesloaded.pkgd.min.js')}}"></script>
-<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{asset('js/owl.carousel.min.js')}}"></script>
-<script src="{{asset('js/jquery.odometer.min.js')}}"></script>
-<script src="{{asset('js/jquery.appear.js')}}"></script>
-<script src="{{asset('js/slick.min.js')}}"></script>
-<script src="{{asset('js/ajax-form.js')}}"></script>
-<script src="{{asset('js/wow.min.js')}}"></script>
-<script src="{{asset('js/aos.js')}}"></script>
-<script src="{{asset('js/plugins.js')}}"></script>
-<script src="{{asset('js/main.js')}}"></script>
-<script>
-    
-    if(loggedIn){
-        const breadcrumb = document.getElementsByClassName("breadcrumb");
-        breadcumb.classList.add("hidden")
 
+<style>#overlay_mb{position:fixed;display:none;width:100%;height:100%;top:0;left:0;right:0;bottom:0;background-color:rgba(0, 0, 0, 0.7);z-index:99999;cursor:pointer}#overlay_mb .overlay_mb_content{position:relative;height:100%}.overlay_mb_block{display:inline-block;position:relative}#overlay_mb .overlay_mb_content .overlay_mb_wrapper{width:600px;height:auto;position:relative;left:50%;top:50%;transform:translate(-50%, -50%);text-align:center}#overlay_mb .overlay_mb_content .cls_ov{color:#fff;text-align:center;cursor:pointer;position:absolute;top:5px;right:5px;z-index:999999;font-size:14px;padding:4px 10px;border:1px solid #aeaeae;background-color:rgba(0, 0, 0, 0.7)}#overlay_mb img{position:relative;z-index:999}@media only screen and (max-width: 768px){#overlay_mb .overlay_mb_content .overlay_mb_wrapper{width:400px;top:3%;transform:translate(-50%, 3%)}}@media only screen and (max-width: 400px){#overlay_mb .overlay_mb_content .overlay_mb_wrapper{width:310px;top:3%;transform:translate(-50%, 3%)}}</style>
+
+<style>
+    #overlay_pc {
+        position: fixed;
+        display: none;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.7);
+        z-index: 99999;
+        cursor: pointer;
     }
-</script>
+    #overlay_pc .overlay_pc_content {
+        position: relative;
+        height: 100%;
+    }
+    .overlay_pc_block {
+        display: inline-block;
+        position: relative;
+    }
+    #overlay_pc .overlay_pc_content .overlay_pc_wrapper {
+        width: 600px;
+        height: auto;
+        position: relative;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
+    #overlay_pc .overlay_pc_content .cls_ov {
+        color: #fff;
+        text-align: center;
+        cursor: pointer;
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        z-index: 999999;
+        font-size: 14px;
+        padding: 4px 10px;
+        border: 1px solid #aeaeae;
+        background-color: rgba(0, 0, 0, 0.7);
+    }
+    #overlay_pc img {
+        position: relative;
+        z-index: 999;
+    }
+    @media only screen and (max-width: 768px) {
+        #overlay_pc .overlay_pc_content .overlay_pc_wrapper {
+            width: 400px;
+            top: 3%;
+            transform: translate(-50%, 3%);
+        }
+    }
+    @media only screen and (max-width: 400px) {
+        #overlay_pc .overlay_pc_content .overlay_pc_wrapper {
+            width: 310px;
+            top: 3%;
+            transform: translate(-50%, 3%);
+        }
+    }
+</style>
+
+<style>
+    .float-ck { position: fixed; bottom: 0px; z-index: 9}
+    * html .float-ck /* IE6 position fixed Bottom */{position:absolute;bottom:auto;top:expression(eval (document.documentElement.scrollTop+document.docum entElement.clientHeight-this.offsetHeight-(parseInt(this.currentStyle.marginTop,10)||0)-(parseInt(this.currentStyle.marginBottom,10)||0))) ;}
+    #hide_float_left a {background: #0098D2;padding: 5px 15px 5px 15px;color: #FFF;font-weight: 700;float: left;}
+    #hide_float_left_m a {background: #0098D2;padding: 5px 15px 5px 15px;color: #FFF;font-weight: 700;}
+    span.bannermobi2 img {height: 70px;width: 300px;}
+    #hide_float_right a { background: #01AEF0; padding: 5px 5px 1px 5px; color: #FFF;float: left;}
+</style>
 </body>
 </html>
