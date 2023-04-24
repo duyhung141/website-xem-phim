89cb3078ac610094ff0866726929128a9e086735
duyhung141 <leduyhung141@gmail.com>
2023-04-24 12:04:09 +0700

    modify user.layout, user.index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,14 +10,9 @@
             <div class="col-xs-12 carausel-sliderWidget">
                 <section id="halim-advanced-widget-4">
                     <div class="section-heading">
-                        <a href={{route('user.movies-option')}} title="Phim Hot">
+                        <a href={{route('user.movies-option')}} title= "Phim Hot">
                             <span class="h-text">Phim Hot</span>
                         </a>
-                        <ul class="heading-nav pull-right hidden-xs">
-                            <li class="section-btn halim_ajax_get_post" data-catid="4" data-showpost="12"
-                                data-widgetid="halim-advanced-widget-4" data-layout="6col"><span
-                                    data-text="Chiếu Rạp"></span></li>
-                        </ul>
                     </div>
                     <div id="halim-advanced-widget-4-ajax-box" class="halim_box">
                         <article class="col-md-2 col-sm-4 col-xs-6 thumb grid-item post-38424">
--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -103,26 +103,14 @@
         <nav class="navbar halim-navbar main-navigation" role="navigation" data-dropdown-hover="1">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse"
-                        data-target="#halim" aria-expanded="false">
+                        data-target="#halim" aria-expanded="true">
                     <span class="sr-only">Menu</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <button type="button" class="navbar-toggle collapsed pull-right expand-search-form"
-                        data-toggle="collapse" data-target="#search-form" aria-expanded="false">
-                    <span class="hl-search" aria-hidden="true"></span>
-                </button>
-                <button type="button" class="navbar-toggle collapsed pull-right get-bookmark-on-mobile">
-                    Bookmarks<i class="hl-bookmark" aria-hidden="true"></i>
-                    <span class="count">0</span>
-                </button>
-                <button type="button" class="navbar-toggle collapsed pull-right get-locphim-on-mobile">
-                    <a href="javascript:;" id="expand-ajax-filter" style="color: #ffed4d;">Lọc <i
-                            class="fas fa-filter"></i></a>
-                </button>
             </div>
-            <div class="collapse navbar-collapse" id="halim">
+            <div class="collapse navbar-collapse" id="halim"  aria-expanded="true">
                 <div class="menu-menu_1-container">
                     <ul id="menu-menu_1" class="nav navbar-nav navbar-left">
                         <li class="current-menu-item active"><a title="Trang Chủ" href={{route('user.index')}}>Trang
@@ -152,14 +140,14 @@
                         <li><a title="Phim Chiếu Rạp" href={{route('user.movies-option')}}>Phim Chiếu Rạp</a></li>
                     </ul>
                 </div>
-                <ul class="nav navbar-nav navbar-left" style="background:#000;">
-                    <li><a href="#" onclick="locphim()" style="color: #ffed4d;">Lọc Phim</a></li>
-                </ul>
+{{--                <ul class="nav navbar-nav navbar-left" style="background:#000;">--}}
+{{--                    <li><a href="#" onclick="locphim()" style="color: #ffed4d;">Lọc Phim</a></li>--}}
+{{--                </ul>--}}
             </div>
         </nav>
-        <div class="collapse navbar-collapse" id="search-form">
-            <div id="mobile-search-form" class="halim-search-form"></div>
-        </div>
+{{--        <div class="collapse navbar-collapse" id="search-form">--}}
+{{--            <div id="mobile-search-form" class="halim-search-form"></div>--}}
+{{--        </div>--}}
         <div class="collapse navbar-collapse" id="user-info">
             <div id="mobile-user-login"></div>
         </div>
@@ -425,6 +413,17 @@
             $('#search-results').html('')
         }
     });
+    // var isMenuOpen = false;
+    // $(".navbar-toggle").click(function(){
+    //     $(this).toggleClass("collapsed");
+    //     if (!isMenuOpen) {
+    //         $("#halim").slideDown();
+    //         isMenuOpen = true;
+    //     } else {
+    //         $("#halim").slideUp();
+    //         isMenuOpen = false;
+    //     }
+    // });
 </script>
 
 </body>
